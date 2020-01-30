53f5208851a5248d0cab2e466bda649e356d766a
alihassanuk <alihassanuk@gmail.com>
2020-01-29 20:17:59 -0500

    first commit

---
+++ MyFirstJava/PackageFirstJava.java
@@ -0,0 +1,24 @@
+package MyFirstJava;
+
+public class PackageFirstJava {
+
+	public static void main(String[] args) {
+	
+		int FirstNumber =15;
+		int SecondNumber = 15;
+		 
+		boolean a = (FirstNumber==SecondNumber);
+		
+		
+		
+		Integer objectNumber = new Integer (6);			
+        Integer objectSecNumber = new Integer (6);
+        
+        boolean b = (objectNumber.equals(objectSecNumber));
+        System.out.println(b);
+        System.out.println(a);	
+	
+	}
+
+}
+
